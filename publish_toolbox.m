c9886396c8b684dfde4fb56e5b183d95060f3833
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2011-10-02 19:14:07 -0400

    for publication

--- publish_toolbox.m
+++
@@ -1,8 +0,0 @@
-clear
-opts.stylesheet = 'doc/agramfort.xsl';
-opts.outputDir = 'doc';
-opts.maxWidth = 600;
-opts.catchError = false;
-opts.evalCode = true;
-
-publish('demo_mesh_processing', opts);
